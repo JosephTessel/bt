dc4283829df0ae3c7524b0e21e79c8c9b747a91b
Joseph Tessel <Tessel.Joseph@gmail.com>
2015-07-20 17:34:06 -0400

    Fix s3 carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :designs
   has_many :votes
+  validates :email, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
